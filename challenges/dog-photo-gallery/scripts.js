1d97567161c07c78da342e8250ca838badb4a65a
Sinead <77743497+Sinead-CYF@users.noreply.github.com>
2021-10-31 21:00:51 +0000

    Updated to arrow functions

--- challenges/dog-photo-gallery/scripts.js
+++ challenges/dog-photo-gallery/scripts.js
@@ -1,21 +1,15 @@
 const imgWrapper = document.querySelector("#img-wrapper");
 const btn1 = document.querySelector("#btn1");
 
-function addImg(){
-    fetch("https://dog.ceo/api/breeds/image/random")
-    .then(function (response){
-        return response.json();
+function addImg() {
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => response.json())
+    .then((data) => {
+      const img = document.querySelector("#img");
+      imgWrapper.style.display = "block";
+      img.src = data.message;
     })
-    .then(function (data){
-        const img = document.querySelector("#img");
-        imgWrapper.style.display = "block";
-        img.src = data.message;
-    })
-    .catch(function (error){
-        console.log(`Error: ${error}`);
-    });
+    .catch((error) => console.log(`Error: ${error}`));
 }
 
 btn1.addEventListener("click", addImg);
-
- 
